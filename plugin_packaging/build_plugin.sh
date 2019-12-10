102046d3c3e5eddd7814d3688753c5cea874efd8
Benjamin Ledel <benjamin@schule-plus.com>
2019-12-10 10:31:08 +0100

    * fix errors with SmartSHARK execution

--- plugin_packaging/build_plugin.sh
+++ plugin_packaging/build_plugin.sh
@@ -2,7 +2,14 @@
 
 current=`pwd`
 mkdir -p /tmp/rMineSHARK/
+cp -R ../gradle /tmp/rMineSHARK
 cp -R ../src /tmp/rMineSHARK
+cp -R ../libs /tmp/rMineSHARK
+cp ../build.gradle /tmp/rMineSHARK 
+cp ../settings.gradle /tmp/rMineSHARK
+cp ../gradlew /tmp/rMineSHARK
+cp ../gradlew.bat /tmp/rMineSHARK
+cp ../gradle.properties /tmp/rMineSHARK
 cp * /tmp/rMineSHARK
 cd /tmp/rMineSHARK/
 
--- plugin_packaging/install.sh
+++ plugin_packaging/install.sh
@@ -6,4 +6,4 @@ cd $PLUGIN_PATH
 # Build jar file
 ./gradlew fatJar
 
-cp build/libs/rMineSHARK*.jar ../build/rMineSHARK.jar
+cp build/libs/rMineSHARK*.jar build/rMineSHARK.jar
